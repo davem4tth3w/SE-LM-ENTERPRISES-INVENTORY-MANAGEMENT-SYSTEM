231740c54e40c63fb6e5fedba99fc25b0f2dc770
KristinDelosSantos <149551857+KristinDelosSantos@users.noreply.github.com>
2024-04-02 15:48:00 +0800

    sell_return_list.php

--- sell_return_list.php
+++ sell_return_list.php
@@ -2,12 +2,16 @@
 // db_config.php
 
 $host = 'your_host'; // e.g., localhost
-$username = 'your_username';
-$password = 'your_password';
-$database = 'erp_system';
+$ID= 'id';
+$category= 'category';
+$brand= 'brand';
+$quantity= 'quantity';
+$return_products= 'return_products';
+$status= 'status';
+$database = 'return_list';
 
 // Create a database connection
-$conn = new mysqli($host, $username, $password, $database);
+$conn = new mysqli($, $id, $category, $brand, $quantity, $return_products, &status, $database);
 
 // Check connection
 if ($conn->connect_error) {
